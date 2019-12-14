32c7968ae22d97d971f672397401e93fd81e3fb4
Fernando Rojo <fernandosrojo@yahoo.com>
2019-12-13 19:27:41 -0500

    exported types

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,5 @@
 export { default as useFocusEffect } from './use-focus-effect'
 export { default as useRouting } from './use-routing'
+
+import * as RouteTypes from './use-routing/types'
+export { RouteTypes }
