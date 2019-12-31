22c91242afd5f46eb6b8f1f076fcb4c56dc2a5a5
Evan Bacon <baconbrix@gmail.com>
2019-12-31 14:39:45 -0800

    remove unused transpile modules

--- examples/next-nav/next.config.js
+++ examples/next-nav/next.config.js
@@ -1,17 +1,10 @@
 const { withExpo } = require('@expo/next-adapter')
 const withFonts = require('next-fonts')
 const withImages = require('next-images')
-const withTM = require('next-transpile-modules')
 const withPlugins = require('next-compose-plugins')
 
 module.exports = withPlugins(
   [
-    [
-      withTM,
-      {
-        transpileModules: ['expo-next-react-navigation'],
-      },
-    ],
     withFonts,
     withImages,
     [withExpo, { projectRoot: __dirname }],
